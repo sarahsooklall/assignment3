a90bab79250e7cb8affb9862f85bf0d16fa5edb2
= <sarahsooklall@yahoo.com>
2018-04-16 17:43:49 -0400

    fixed api url

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 /*
-** Button to start quiz
-*/
-const startButton = document.getElementById('start');
-startButton.onclick = function (){
-    document.getElementById('partTwo').style.display = "block";
-    document.getElementById('partOne').style.display = "none";
-}
-
-/*
 ** Document ready event
 */
 $(document).ready(function() {
@@ -17,7 +8,7 @@ $(document).ready(function() {
 			var offset = Math.round(Math.random() * 1000);
 			var query = this.value;
 			var key = "G27cFqltoAFa6grAhM6gJv94Nbk5zdiL";
-			var url = "http://api.giphy.com/v1/gifs/search?q="
+			var url = "https://api.giphy.com/v1/gifs/search?q="
 						+ query
 						+ "&api_key="
 						+ key
@@ -28,15 +19,29 @@ $(document).ready(function() {
 			$.getJSON(url, function(json) {
                 
                 /*
-                ** Basic Identifying ID
+                ** Identifying ID
                 */
                 document.getElementById('gifs').innerHTML = "";
-                const result = document.getElementById('endResult');
-                const message = document.createElement('article');
-                const container = document.getElementById("partTwo");
+                document.getElementById('endResult').innerHTML = "";
+                document.getElementById('message').innerHTML = "";
+                document.getElementById('partTwo').innerHTML = "";
+//                const result = document.getElementById('endResult');
+//                const message = document.createElement('article');
+//                const second = document.getElementById('partTwo');
                 
                 /* Score, set to zero - to begin at zero */
                 var click = 0;
+                
+                /*
+                ** Button to start quiz
+                */
+                document.getElementById('start').innerHTML = "";
+                //const startButton = document.getElementById('start');
+                $('#start').click(function() {
+                    //startButton.onclick = function (){
+                    document.getElementById('partTwo').style.display = "block";
+                    document.getElementById('partOne').style.display = "none";
+                }
                 
                 /*
                 ** Gif Code
@@ -71,31 +76,42 @@ $(document).ready(function() {
                     
                     /* Statment to display message when gif is clicked a certain amount*/
                     if (click++ > 11){ //click 10
-                        message.textContent ="You clicked 10 times!";
 //                        document.getElementById('partTwo').style.display = "block";
 //                        document.getElementById('endResult').style.display = "none";
-                        }else if(click++ > 21){ //click 20
-                            message.textContent ="You clicked 20 times!";
+                        console.log("You clicked 10 times!");
+                        //Question
+                        message.textContent ="Why did Kevin take soap to school!";
 //                            document.getElementById('partTwo').style.display = "block";
 //                            document.getElementById('endResult').style.display = "none";
+                            console.log("You clicked 20 times!");
+                            //Answer
+                            message.textContent ="Because the weather-man perdicted showers!";
                         }else if(click++ > 31){ //click 30
-                            message.textContent ="You clicked 30 times!";
+                            console.log("You clicked 30 times!");
+                            //Question
                         }else if(click++ > 41){ //click 40
-                            message.textContent ="You clicked 40 times!";
+                            console.log("You clicked 40 times!");
+                            //Answer
                         }else if(click++ > 51){ //click 50
-                            message.textContent ="You clicked 50 times!";
+                            console.log("You clicked 50 times!");
+                            //Question
                         }else if(click++ > 61){ //click 60
-                            message.textContent ="You clicked 60 times!";
+                            console.log("You clicked 60 times!");
+                            //Answer
                         }else if(click++ > 71){ //click 70
-                            message.textContent ="You clicked 70 times!";
+                            console.log("You clicked 70 times!");
+                            //Question
                         }else if(click++ > 81){ //click 80
-                            message.textContent ="You clicked 80 times!";
+                            console.log("You clicked 80 times!");
+                            //Answer
                         }else if(click++ > 91){ //click 90
-                            message.textContent ="You clicked 90 times!";
-                    }else{ //click 100
+                            console.log("You clicked 90 times!");
+                            //Question
+                    }else{ //click 100+
                         document.getElementById('partTwo').style.display = "none";
                         document.getElementById('endResult').style.display = "block";
-                        message.textContent ="You clicked 100 times!";
+                        console.log("You clicked 100 or more times!");
+                        //Answer
                     }
                     
                 });
