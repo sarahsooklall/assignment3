1fb17701d062d3d9b7a1ba52b953e215e9557e17
vukanmrvaljevic <31517206+vukanmrvaljevic@users.noreply.github.com>
2018-04-16 20:08:37 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 ** Document ready event
 */
 $(document).ready(function() {
+    
+     $('#start').click(function() {
+                    //startButton.onclick = function (){
+                    document.getElementById('partTwo').style.display = "block";
+                    document.getElementById('partOne').style.display = "none";
+                });
+    
 	$('#query').on("keypress", function(event) {
         /* keyCode 13 is the enter key to submit query */
 		if (event.which == 13) {
@@ -22,13 +29,10 @@ $(document).ready(function() {
                 ** Identifying ID
                 */
                 document.getElementById('gifs').innerHTML = "";
-                document.getElementById('endResult').innerHTML = "";
-                document.getElementById('message').innerHTML = "";
-                document.getElementById('partTwo').innerHTML = "";
-//                const result = document.getElementById('endResult');
-//                const message = document.createElement('article');
-//                const second = document.getElementById('partTwo');
-                
+//                document.getElementById('endResult').innerHTML = "";
+                const message = document.createElement('article');
+                const partTwo = document.getElementById('partTwo');
+                const result = document.getElementById('endResult');
                 /* Score, set to zero - to begin at zero */
                 var click = 0;
                 
@@ -37,11 +41,6 @@ $(document).ready(function() {
                 */
                 document.getElementById('start').innerHTML = "";
                 //const startButton = document.getElementById('start');
-                $('#start').click(function() {
-                    //startButton.onclick = function (){
-                    document.getElementById('partTwo').style.display = "block";
-                    document.getElementById('partOne').style.display = "none";
-                }
                 
                 /*
                 ** Gif Code
@@ -66,7 +65,7 @@ $(document).ready(function() {
                 ** Clickable Gif Code
                 */
                 $('#gifs').click(function() {
-                    const $gifs = $(this);
+//                 const $gifs = $(this);
                     
                     /* To increment score by 1 when clicking */
 					click++;
@@ -74,38 +73,59 @@ $(document).ready(function() {
                     /* Showing the message for last page */
                     $('#endResult').append(message);
                     
+                    
                     /* Statment to display message when gif is clicked a certain amount*/
-                    if (click++ > 11){ //click 10
+                    if (click == 10){ //click 10
+                        const result = document.createElement('p');
 //                        document.getElementById('partTwo').style.display = "block";
 //                        document.getElementById('endResult').style.display = "none";
                         console.log("You clicked 10 times!");
                         //Question
                         message.textContent ="Why did Kevin take soap to school!";
+                    } else if(click < 21){
 //                            document.getElementById('partTwo').style.display = "block";
 //                            document.getElementById('endResult').style.display = "none";
                             console.log("You clicked 20 times!");
                             //Answer
                             message.textContent ="Because the weather-man perdicted showers!";
-                        }else if(click++ > 31){ //click 30
+                        }else if(click < 31){ //click 30
+                            message.textContent = "When will water stop running down hill?";
                             console.log("You clicked 30 times!");
+                             //Answer
+                            message.textContent ="When it reaches the bottom.";
                             //Question
-                        }else if(click++ > 41){ //click 40
+                        }else if(click < 41){ //click 40
+                            message.textContent ="Which is the only way a leopard can change his spots?";
                             console.log("You clicked 40 times!");
+                             //Answer
+                            message.textContent ="By going from one spot to another.";
                             //Answer
-                        }else if(click++ > 51){ //click 50
+                        }else if(click < 51){ //click 50
+                            message.textContent ="What do you call two witches who live together?";
                             console.log("You clicked 50 times!");
+                             //Answer
+                            message.textContent ="Broommates!";
                             //Question
-                        }else if(click++ > 61){ //click 60
+                        }else if(click < 61){ //click 60
+                            message.textContent ="There are two monkeys on a tree and one jumps off.  Why does the other monkey jump too?";
                             console.log("You clicked 60 times!");
                             //Answer
-                        }else if(click++ > 71){ //click 70
+                            message.textContent ="Monkey see monkey do.";
+                        }else if(click < 71){ //click 70
+                            message.textContent ="What can point in every direction but can't reach the destination by itself.";
                             console.log("You clicked 70 times!");
+                             //Answer
+                            message.textContent ="Your finger.";
                             //Question
-                        }else if(click++ > 81){ //click 80
+                        }else if(click < 81){ //click 80
+                            message.textContent ="When is music like vegetables?";
                             console.log("You clicked 80 times!");
-                            //Answer
-                        }else if(click++ > 91){ //click 90
+                             //Answer
+                            message.textContent ="When there are two beats (beets) to the measure.";
+                        }else if(click < 91){ //click 90
+                            message.textContent ="What did the comedian say when he took off his clothes?";
                             console.log("You clicked 90 times!");
+                            message.textContent ="Haven't you ever seen a comic strip.";
                             //Question
                     }else{ //click 100+
                         document.getElementById('partTwo').style.display = "none";
@@ -120,4 +140,3 @@ $(document).ready(function() {
 		}
 	});
 });
-              
